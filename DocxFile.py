0a6dd95c77176015d171f4fb7ee18d9298adb69c
Jeferson-Peter <jefersonpeter32@gmail.com>
2021-07-08 22:22:28 -0300

    Refactoring in classes

---
+++ DocxFile.py
@@ -0,0 +1,23 @@
+from os import walk
+import os
+import shutil
+import Paths
+class DocxFile:
+    def executeTransfer(self, main_path):
+        path = Paths.ReadPath()
+        files = []
+
+        for (dirpath, dirnames, filenames) in walk(main_path):
+            # files.extend(filenames)
+            print(f'files.extend(filenames): {files.extend(filenames)}')
+            print(f'filenames: {filenames}')
+            print(f'files: {files}')
+            break
+        for file in files:
+            if not file.__contains__(".docx") and not file.__contains__(".pdf") and not file.__contains__(".txt"):
+                continue
+            print(f'2: {file}')
+            if file.__contains__(".docx"):
+                old_file_path = os.path.join(main_path, file)
+                new_file_path = os.path.join(path.docxPath(), file)
+                shutil.move(old_file_path, new_file_path)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import QuantumTransfer
 import Paths
+import DocxFile
+import PdfFile
+import TxtFile
 
 if __name__ == '__main__':
     path = Paths.ReadPath()
@@ -12,9 +15,36 @@ if __name__ == '__main__':
                             ou 'Documents' para executar tranferência de arquivo em documentos: '''
                         )).lower()
         if option == 'dowload':
-            transfer.executeTransfer(path.dowload(), path.docxPath(), path.pdfPath(), path.txtPath())
+            while input_user != 'S':
+                optionMove = int(input(''' Digite 
+                1 para PDF
+                2 para DOCX
+                3 para TXT: '''))
+                if optionMove == 1:
+                    PdfFile.PdfFile.executeTransfer(path.dowload())
+                elif optionMove == 2:
+                    DocxFile.DocxFile.executeTransfer(path.dowload())
+                elif optionMove == 3:
+                    TxtFile.TxtFile.executeTransfer(path.dowload())
+                else:
+                    print("Invalid Option")
+                break
         elif option == 'documents':
-            transfer.executeTransfer(path.documents(), path.docxPath(), path.pdfPath(), path.txtPath())
+            while input_user != 'S':
+                optionMove = int(input(''' Digite 
+                1 para PDF
+                2 para DOCX
+                3 para TXT: '''))
+                print(optionMove == 1)
+                if optionMove == 1:
+                    PdfFile.PdfFile.executeTransfer(path.documents())
+                elif optionMove == 2:
+                    DocxFile.DocxFile.executeTransfer(path.documents())
+                elif optionMove == 3:
+                    TxtFile.TxtFile.executeTransfer(path.documents())
+                else:
+                    print("Invalid Option")
+                break
         else:
             print("Opção Inválida")
             break
