89de37747e0585258afcc7b46a2f777cb652e151
Jeferson-Peter <jefersonpeter32@gmail.com>
2021-07-09 21:23:06 -0300

    Refatoring main class to parameter function pass

--- main.py
+++ main.py
@@ -10,29 +10,18 @@ def run():
     txt = TxtFile.TxtFile()
     option = input(str('''
     Digite 'Dowload' para executar transferências de arquivos neste local,
-                        ou 'Documents' para executar tranferência de arquivo em documentos: '''
+    ou 'Documents' para executar tranferência de arquivo em documentos: '''
                 )).lower()
-    optionMove = int(input(''' Digite 
-    1 para PDF
-    2 para DOCX
-    3 para TXT: '''))
-    print(f"optionMove {optionMove}")
-    print(f"option {option}")
-    print(f"path.documents(): {path.documents()}")
-    documents = path.documents()
-    download = path.dowload()
-    if optionMove == 1 and option == 'dowload':
-        pdf.executeTransfer(download)
-    elif optionMove == 2 and option == 'dowload':
-        docx.executeTransfer(download)
-    elif optionMove == 3 and option == 'dowload':
-        txt.executeTransfer(download)
-    if optionMove == 1 and option == 'documents':
-        pdf.executeTransfer(documents)
-    elif optionMove == 2 and option == 'documents':
-        docx.executeTransfer(documents)
-    elif optionMove == 3 and option == 'documents':
-        txt.executeTransfer(documents)
+    optionMove = int(input(''' Digite :
+        1 para PDF
+        2 para DOCX
+        3 para TXT: '''))
+    if optionMove == 1 and option == 'dowload': pdf.executeTransfer(path.dowload())
+    elif optionMove == 2 and option == 'dowload':docx.executeTransfer(path.dowload())
+    elif optionMove == 3 and option == 'dowload':txt.executeTransfer(path.dowload())
+    if optionMove == 1 and option == 'documents':pdf.executeTransfer(path.documents())
+    elif optionMove == 2 and option == 'documents':docx.executeTransfer(path.documents())
+    elif optionMove == 3 and option == 'documents':txt.executeTransfer(path.documents())
     else: print("Invalid Option")
 
 if __name__ == '__main__':
