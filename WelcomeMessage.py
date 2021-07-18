3e1fe8dfad3576fa1c76437c758a1b4c6a646762
Jeferson-Peter <jefersonpeter32@gmail.com>
2021-07-18 02:19:23 -0300

    Whole refactor in project

--- WelcomeMessage.py
+++ WelcomeMessage.py
@@ -1,6 +1,16 @@
+import webbrowser
 def welcomeMessage():
-    print("_____________________________________________________")
-    print("______ WELCOME TO THE TRANSFER FILES PROGRAMS________")
-    print("______ HERE YOU WILL BE ABLE TO TRANSFER FILES_______")
-    print("______    PDF, DOCX OR TXT'S FILES ARE ABLE   _______")
-    print("_____________________________________________________")
+    print("______________________________________________________________________")
+    print("______ WELCOME TO THE TRANSFER FILES PROGRAMS_________________________")
+    print("______ HERE YOU WILL BE ABLE TO TRANSFER FILES________________________")
+    print("______    PDF, DOCX OR TXT'S FILES ARE ABLE   ________________________")
+    print("______________________________________________________________________")
+def goodByeMessage():
+    link = "https://github.com/Jeferson-Peter"
+    print("______________________________________________________________________")
+    print("___________________  Program Made By Jeferson-Peter  _________________")
+    print("___________________   © 2021 Jeferson-Peter, Inc.    _________________")
+    print("___________________   Para ver mais sobre meu trab   _________________")
+    print("___________________              Acesse              _________________")
+    print('__________________  https://github.com/Jeferson-Peter ________________')
+    print("______________________________________________________________________")
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import Paths
-import DocxFile
-import PdfFile
-import TxtFile
+from transferPaths import DocxFile, PdfFile, ZipEncode, TxtFile
 import WelcomeMessage
-import ZipEncode
+
+
 def run():
     path = Paths.ReadPath()
     docx = DocxFile.DocxFile()
@@ -13,22 +12,33 @@ def run():
     option = int(input('''
     Digite 1 para executar transferências de arquivos para downloads,
     ou 2 para executar tranferência de arquivo em documentos: '''
-                ))
+                       ))
     optionMove = int(input(''' Digite :
         1 para PDF
         2 para DOCX
         3 para TXT
         4 para ZIP: '''))
-    if optionMove == 1 and option == 1: pdf.executeTransfer(path.dowload())
-    elif optionMove == 2 and option == 1:docx.executeTransfer(path.dowload())
-    elif optionMove == 3 and option == 1:txt.executeTransfer(path.dowload())
-    elif optionMove == 4 and option == 1:zip.executeTransfer(path.dowload())
-    if optionMove == 1 and option == 2:pdf.executeTransfer(path.documents())
-    elif optionMove == 2 and option == 2:docx.executeTransfer(path.documents())
-    elif optionMove == 3 and option == 2:txt.executeTransfer(path.documents())
-    elif optionMove == 4 and option == 2:zip.executeTransfer(path.documents())
-    else: print("Invalid Option")
+    if optionMove == 1 and option == 1:
+        pdf.executeTransfer(path.dowload())
+    elif optionMove == 2 and option == 1:
+        docx.executeTransfer(path.dowload())
+    elif optionMove == 3 and option == 1:
+        txt.executeTransfer(path.dowload())
+    elif optionMove == 4 and option == 1:
+        zip.executeTransfer(path.dowload())
+    if optionMove == 1 and option == 2:
+        pdf.executeTransfer(path.documents())
+    elif optionMove == 2 and option == 2:
+        docx.executeTransfer(path.documents())
+    elif optionMove == 3 and option == 2:
+        txt.executeTransfer(path.documents())
+    elif optionMove == 4 and option == 2:
+        zip.executeTransfer(path.documents())
+    else:
+        print("Invalid Option")
+
 
 if __name__ == '__main__':
     WelcomeMessage.welcomeMessage()
     run()
+    WelcomeMessage.goodByeMessage()
