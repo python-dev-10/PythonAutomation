3731442f8f265d14b3b5b8f1060d3f0359fe8cbb
Jeferson <jefersonpeter32@gmail.com>
2021-08-08 22:21:17 -0300

    Refactor

--- customMessages/CustomMessage.py
+++ customMessages/CustomMessage.py
@@ -2,7 +2,7 @@ from colorama import Fore
 class CustomMessage:
 
     def errorMessage(self, content):
-        raise(f"{Fore.RED}{content}{Fore.RESET}")
+        return print(f"{Fore.RED}{content}{Fore.RESET}")
 
     def successMessage(self, content):
         return print(f"{Fore.GREEN}{content}{Fore.RESET}")
